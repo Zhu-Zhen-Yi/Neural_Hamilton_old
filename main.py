286742cd8fe2de5608fbc67984b3dd21721befa5
axect <axect@outlook.kr>
2024-05-07 15:07:27 +0900

    VAE

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from optuna.visualization import plot_optimization_history, plot_param_importanc
 # PyTorch for deep learning
 import torch
 from torch.optim import Adam
-from torch.optim.lr_scheduler import OneCycleLR, PolynomialLR, CosineAnnealingLR
+from torch.optim.lr_scheduler import PolynomialLR
 from torch.utils.data import DataLoader
 import lightning as L
 
@@ -20,7 +20,7 @@ from rich.console import Console
 from rich.progress import Progress
 
 # Neural Hamilton modules
-from neural_hamilton.model import DeepONet
+from neural_hamilton.model import DeepONet, VAONet
 from neural_hamilton.data import train_dataset, val_dataset
 from neural_hamilton.train import train_epoch, evaluate
 
@@ -43,23 +43,34 @@ def parse_arguments():
 def objective(trial, console, progress, task_id):
     L.seed_everything(42)
 
+    # hparams = {
+    #     "num_input": 100,
+    #     "num_branch": trial.suggest_int("num_branch", 1, 4) * 10,
+    #     "num_output": 100,
+    #     "dim_output": 1,
+    #     "hidden_size": 2 ** trial.suggest_int("hidden_size", 5, 7),
+    #     "branch_hidden_depth": trial.suggest_int("branch_hidden_depth", 2, 10),
+    #     "trunk_hidden_depth": trial.suggest_int("trunk_hidden_depth", 2, 10),
+    #     "learning_rate": trial.suggest_float("learning_rate", 1e-3, 5e-2, log=True),
+    #     "batch_size": 5000,
+    #     "epochs": 200,
+    # }
     hparams = {
-        "num_input": 100,
-        "num_branch": trial.suggest_int("num_branch", 1, 4) * 10,
-        "num_output": 100,
-        "dim_output": 1,
-        "hidden_size": 2 ** trial.suggest_int("hidden_size", 5, 7),
-        "branch_hidden_depth": trial.suggest_int("branch_hidden_depth", 2, 10),
-        "trunk_hidden_depth": trial.suggest_int("trunk_hidden_depth", 2, 10),
+        "hidden_size": trial.suggest_int("hidden_size", 10, 80),
+        "num_layers": trial.suggest_int("num_layers", 1, 3),
         "learning_rate": trial.suggest_float("learning_rate", 1e-3, 5e-2, log=True),
-        "batch_size": 5000,
-        "epochs": 200,
+        "batch_size": 2000,
+        "epochs": 100,
+        "dropout": 0.1,
+        "latent_size": trial.suggest_int("latent_size", 10, 80),
+        "kl_weight": trial.suggest_float("kl_weight", 1e-3, 1e+0, log=True),
     }
 
     dl_train = DataLoader(ds_train, batch_size=hparams["batch_size"], shuffle=True)
     dl_val = DataLoader(ds_val, batch_size=hparams["batch_size"], shuffle=False)
 
-    model = DeepONet(hparams)
+    # model = DeepONet(hparams)
+    model = VAONet(hparams)
     model.to(device)
 
     optimizer = Adam(model.parameters(), lr=hparams["learning_rate"])
@@ -70,14 +81,21 @@ def objective(trial, console, progress, task_id):
         os.makedirs(checkpoint_dir)
 
     try:
-        run = wandb.init(project="NeuralHamilton-GRF-Optuna", config=hparams, reinit=False)
+        run = wandb.init(project="NeuralHamilton-VAE-Optuna", config=hparams, reinit=False)
         progress_epoch = progress.add_task("[cyan]Epochs", total=hparams["epochs"])
         for epoch in range(hparams["epochs"]):
-            train_loss = train_epoch(model, optimizer, dl_train, device)
-            val_loss = evaluate(model, dl_val, device)
-            wandb.log({"train_loss": train_loss, "val_loss": val_loss, "epoch": epoch+1, "lr": scheduler.get_last_lr()[0]})
+            train_loss, train_kl_loss = train_epoch(model, optimizer, dl_train, device)
+            val_loss, val_kl_loss = evaluate(model, dl_val, device)
             scheduler.step()
             progress.update(progress_epoch, advance=1)
+            wandb.log({
+                "train_loss": train_loss,
+                "train_kl_loss": train_kl_loss,
+                "val_loss": val_loss,
+                "val_kl_loss": val_kl_loss,
+                "epoch": epoch+1, 
+                "lr": scheduler.get_last_lr()[0]
+            })
 
             if trial.should_prune():
                 raise optuna.TrialPruned()
@@ -99,7 +117,7 @@ def main():
     args = parse_arguments()
 
     sampler = TPESampler(seed=42)
-    pruner = MedianPruner(n_startup_trials=10, n_warmup_steps=10, interval_steps=5)
+    pruner = MedianPruner(n_startup_trials=10, n_warmup_steps=5)
 
     console = Console()
     progress = Progress()
@@ -108,13 +126,13 @@ def main():
         task_id = progress.add_task("[green]Optuna trials", total=args.n_trials)
 
         study = optuna.create_study(
-            study_name="NeuralHamilton-GRF",
+            study_name="NeuralHamilton-VAE",
             storage="sqlite:///NeuralHamilton.db",
             sampler=sampler,
             pruner=pruner,
             direction="minimize",
             load_if_exists=True
-            )
+        )
         study.optimize(lambda trial: objective(trial, console, progress, task_id), n_trials=args.n_trials)
 
     print("Best trial:")
--- neural_hamilton/train.py
+++ neural_hamilton/train.py
@@ -1,26 +1,73 @@
 import torch
 import torch.nn.functional as F
 
+# def train_epoch(model, optimizer, dataloader, device):
+#     model.train()
+#     epoch_loss = 0
+#     for u, y, Guy in dataloader:  
+#         optimizer.zero_grad()
+#         pred = model(u.to(device), y.to(device))
+#         loss = F.mse_loss(pred, Guy.to(device))
+#         loss.backward()
+#         optimizer.step()
+#         epoch_loss += loss.item()
+#     epoch_loss /= len(dataloader)
+#     return epoch_loss
+
+# def evaluate(model, dataloader, device):
+#     model.eval()  
+#     eval_loss = 0
+#     with torch.no_grad():
+#         for u, y, Guy in dataloader:
+#             pred = model(u.to(device), y.to(device))
+#             loss = F.mse_loss(pred, Guy.to(device)) 
+#             eval_loss += loss.item()
+#     eval_loss /= len(dataloader)
+#     return eval_loss
+
 def train_epoch(model, optimizer, dataloader, device):
     model.train()
     epoch_loss = 0
+    epoch_kl_loss = 0
     for u, y, Guy in dataloader:  
         optimizer.zero_grad()
-        pred = model(u.to(device), y.to(device))
-        loss = F.mse_loss(pred, Guy.to(device))
+        pred, mu, logvar = model(u.to(device), y.to(device))
+
+        # Flatten
+        mu_vec = mu.view((mu.shape[0], -1))             # B, D * L * Z
+        logvar_vec = logvar.view((logvar.shape[0], -1)) # B, D * L * Z
+
+        # KL Divergence (mean over latent dimensions)
+        kl_loss = -0.5 * torch.mean(1 + logvar_vec - mu_vec.pow(2) - logvar_vec.exp(), dim=1)
+        kl_loss = model.kl_weight * torch.mean(kl_loss)
+        loss = F.mse_loss(pred, Guy.to(device)) + kl_loss
         loss.backward()
         optimizer.step()
         epoch_loss += loss.item()
+        epoch_kl_loss += kl_loss.item()
     epoch_loss /= len(dataloader)
-    return epoch_loss
+    epoch_kl_loss /= len(dataloader)
+    return epoch_loss, epoch_kl_loss
 
 def evaluate(model, dataloader, device):
     model.eval()  
     eval_loss = 0
+    eval_kl_loss = 0
     with torch.no_grad():
         for u, y, Guy in dataloader:
-            pred = model(u.to(device), y.to(device))
-            loss = F.mse_loss(pred, Guy.to(device)) 
+            pred, mu, logvar = model(u.to(device), y.to(device))
+            
+            # Flatten
+            mu_vec = mu.view((mu.shape[0], -1))             # B, D * L * Z
+            logvar_vec = logvar.view((logvar.shape[0], -1)) # B, D * L * Z
+
+            # KL Divergence (mean over latent dimensions)
+            kl_loss = -0.5 * torch.mean(1 + logvar_vec - mu_vec.pow(2) - logvar_vec.exp(), dim=1)
+            kl_loss = model.kl_weight * torch.mean(kl_loss)
+            loss = F.mse_loss(pred, Guy.to(device)) + kl_loss
+            
             eval_loss += loss.item()
+            eval_kl_loss += kl_loss.item()
     eval_loss /= len(dataloader)
-    return eval_loss
+    eval_kl_loss /= len(dataloader)
+    return eval_loss, eval_kl_loss
